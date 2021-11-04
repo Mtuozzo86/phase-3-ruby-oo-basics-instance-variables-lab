f9c918918067c68d456866210c096c0746994bfe
Mtuozzo86 <Mtuozzo86@gmail.com>
2021-11-04 15:31:52 -0400

    commit tree 728a5af9e763aee069d35f0c5ecd0ba50ca042a5

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,17 @@
-class Dog
+require "pry"
 
+class Dog
+  #setter method
+  def name=(dog_name) # name= (name equals) method.  Takes in an argument and assigns it to the variable
+    @this_dogs_name = dog_name
+  end
+
+  def name # this method reports the name
+    @this_dogs_name
+  end
 end
+
+lassie = Dog.new
+lassie.name = "Lassie"
+
+puts
